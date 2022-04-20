14df8f68706d30113ecec8b6c84dc8f56d6d03eb
Malcolm Nixon <Malcolm.nixon@gmail.com>
2022-04-19 21:27:41 -0400

    Updated VERSIONS.md to describe turn provider change
    Extracted turning logic from Function_Direct_movement into a separate movement provider.

--- addons/godot-xr-tools/functions/Function_Direct_movement.gd
+++ addons/godot-xr-tools/functions/Function_Direct_movement.gd
@@ -11,8 +11,6 @@ extends MovementProvider
 ##     attached to the players ARVROrigin.
 ##
 ##     The following types of direct movement are supported:
-##      - Snap turning
-##      - Smooth turning
 ##      - Slewing
 ##      - Forwards and backwards motion
 ##
@@ -20,34 +18,20 @@ extends MovementProvider
 ##     controllers to provide different types of direct movement.
 ##
 
-enum MOVEMENT_TYPE { MOVE_AND_ROTATE, MOVE_AND_STRAFE }
 
 ## Movement provider order
 export var order := 10
 
-## Use smooth rotation (may cause motion sickness)
-export var smooth_rotation := false
-
-## Smooth turn speed in radians per second
-export var smooth_turn_speed := 2.0
-
-## Seconds per step (at maximum turn rate)
-export var step_turn_delay := 0.2
-
-## Step turn angle in degrees
-export var step_turn_angle := 20.0
-
 ## Movement speed
 export var max_speed := 10.0
 
-## Type of movement to perform
-export (MOVEMENT_TYPE) var move_type = MOVEMENT_TYPE.MOVE_AND_ROTATE
+## Enable player strafing
+export var strafe := false
 
-# Turn step accumulator
-var _turn_step := 0.0
 
 # Controller node
 onready var _controller : ARVRController = get_parent()
+
 
 # Perform jump movement
 func physics_movement(delta: float, player_body: PlayerBody):
@@ -55,54 +39,17 @@ func physics_movement(delta: float, player_body: PlayerBody):
 	if !_controller.get_is_active():
 		return
 
-	# Handle rotation
-	if move_type == MOVEMENT_TYPE.MOVE_AND_ROTATE:
-		_perform_player_rotation(delta, player_body)
-
 	# Apply forwards/backwards ground control
 	player_body.ground_control_velocity.y += _controller.get_joystick_axis(1) * max_speed
 
 	# Apply left/right ground control
-	if move_type == MOVEMENT_TYPE.MOVE_AND_STRAFE:
+	if strafe:
 		player_body.ground_control_velocity.x += _controller.get_joystick_axis(0) * max_speed
 
 	# Clamp ground control
 	player_body.ground_control_velocity.x = clamp(player_body.ground_control_velocity.x, -max_speed, max_speed)
 	player_body.ground_control_velocity.y = clamp(player_body.ground_control_velocity.y, -max_speed, max_speed)
 
-# Perform rotation based on the players rotation controller input
-func _perform_player_rotation(delta: float, player_body: PlayerBody):
-	var left_right := _controller.get_joystick_axis(0)
-	
-	if abs(left_right) <= 0.1:
-		# Not turning
-		_turn_step = 0.0
-		return
-
-	# Handle smooth rotation
-	if smooth_rotation:
-		_rotate_player(player_body, smooth_turn_speed * delta * left_right)
-		return
-
-	# Update the next turn-step delay
-	_turn_step -= abs(left_right) * delta
-	if _turn_step >= 0.0:
-		return
-
-	# Turn one step in the requested direction
-	_turn_step = step_turn_delay
-	_rotate_player(player_body, deg2rad(step_turn_angle) * sign(left_right))
-
-# Rotate the origin node around the camera
-func _rotate_player(player_body: PlayerBody, angle: float):
-	var t1 := Transform()
-	var t2 := Transform()
-	var rot := Transform()
-
-	t1.origin = -player_body.camera_node.transform.origin
-	t2.origin = player_body.camera_node.transform.origin
-	rot = rot.rotated(Vector3(0.0, -1.0, 0.0), angle)
-	player_body.origin_node.transform = (player_body.origin_node.transform * t2 * rot * t1).orthonormalized()
 
 # This method verifies the MovementProvider has a valid configuration.
 func _get_configuration_warning():
