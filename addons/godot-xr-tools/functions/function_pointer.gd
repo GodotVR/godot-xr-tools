19b6fd307e16913614bdcfd703f94465bb0b26cc
Malcolm Nixon <Malcolm.nixon@gmail.com>
2022-10-13 22:37:48 -0400

    Clean up objects in demo scenes. (#202)
    Added throwing-hammer object to pickable demo
    
    Cleanup of function_pointer script to match 4.0-dev Modified loading_screen shader to take cutout as parameter
    
    Merged function_teleport 3.x and 4.x scripts Merged viewport_2d_in_3d 3.x and 4.x scripts
    
    Fixed white space

--- addons/godot-xr-tools/functions/function_pointer.gd
+++ addons/godot-xr-tools/functions/function_pointer.gd
@@ -2,6 +2,24 @@ tool
 class_name XRToolsFunctionPointer, "res://addons/godot-xr-tools/editor/icons/function.svg"
 extends Spatial
 
+
+##
+## Pointer Function Script
+##
+## @desc:
+##     This script implements a pointer function for a players controller. The
+##     pointer supports sending signals to XRToolsInteractableArea or
+##     XRToolsInteractableBody objects.
+##
+##     The following signals are sent to these objects:
+##      - pointer_pressed(at) with the pointer location
+##      - pointer_released(at) with the pointer location
+##      - pointer_moved(from, to) with the pointer movement
+##      - pointer_entered()
+##      - pointer_exited()
+##
+
+
 # enum our buttons, should find a way to put this more central
 enum Buttons {
 	VR_BUTTON_BY = 1,
@@ -22,124 +40,94 @@ enum Buttons {
 	VR_ACTION = 255
 }
 
-export var enabled = true setget set_enabled
-export var show_laser = true setget set_show_laser
-export var show_target = false
-export (Buttons) var active_button = Buttons.VR_TRIGGER
+
+## Pointer enabled property
+export var enabled : bool = true setget set_enabled
+
+## Show laser property
+export var show_laser : bool = true setget set_show_laser
+
+## Show laser target
+export var show_target : bool = false
+
+## Y Offset for pointer
+export var y_offset : float = -0.05 setget set_y_offset
+
+## Pointer distance
+export var distance : float = 10 setget set_distance
+
+## Pointer collision mask
+export (int, LAYERS_3D_PHYSICS) var collision_mask : int = 15 setget set_collision_mask
+
+## Enable pointer collision with bodies
+export var collide_with_bodies : bool = true setget set_collide_with_bodies
+
+## Enable pointer collision with areas
+export var collide_with_areas : bool = false setget set_collide_with_areas
+
+## Active button
+export (Buttons) var active_button : int = Buttons.VR_TRIGGER
+
+## Action to monitor (if button set to VR_ACTION)
 export var action = ""
-export var y_offset = -0.05 setget set_y_offset
-export var distance = 10 setget set_distance
-export (int, LAYERS_3D_PHYSICS) var collision_mask = 15 setget set_collision_mask
-export var collide_with_bodies = true setget set_collide_with_bodies
-export var collide_with_areas = false setget set_collide_with_areas
 
-var target = null
-var last_target = null
-var last_collided_at = Vector3(0, 0, 0)
 
-var ws = 1.0
+# Current target
+var target : Spatial
 
-func set_enabled(p_enabled):
-	enabled = p_enabled
+# Last target
+var last_target : Spatial
 
-	# this gets called before our scene is ready, we'll call this again in _ready to enable this
-	if is_inside_tree():
-		$Laser.visible = p_enabled and show_laser
-		$RayCast.enabled = p_enabled
+# Last collision point
+var last_collided_at : Vector3 = Vector3.ZERO
 
-func set_show_laser(p_show):
-	show_laser = p_show
-	if is_inside_tree():
-		$Laser.visible = enabled and show_laser
+# World scale
+var ws : float = 1.0
 
-func set_y_offset(p_offset):
-	y_offset = p_offset
-	if is_inside_tree():
-		$Laser.translation.y = y_offset * ws
-		$RayCast.translation.y = y_offset * ws
 
-func set_collision_mask(p_new_mask):
-	collision_mask = p_new_mask
-	if is_inside_tree():
-		$RayCast.collision_mask = collision_mask
-
-func set_distance(p_new_value):
-	distance = p_new_value
-	if is_inside_tree():
-		$Laser.mesh.size.z = distance
-		$Laser.translation.z = distance * -0.5
-		$RayCast.cast_to.z = -distance
-
-func set_collide_with_bodies(p_new_value : bool):
-	collide_with_bodies = p_new_value
-	if is_inside_tree():
-		$RayCast.collide_with_bodies = collide_with_bodies
-
-func set_collide_with_areas(p_new_value : bool):
-	collide_with_areas = p_new_value
-	if is_inside_tree():
-		$RayCast.collide_with_areas = collide_with_areas
-
-func _button_pressed():
-	if $RayCast.is_colliding():
-		target = $RayCast.get_collider()
-		last_collided_at = $RayCast.get_collision_point()
-
-		if target.has_signal("pointer_pressed"):
-			target.emit_signal("pointer_pressed", last_collided_at)
-		elif target.has_method("pointer_pressed"):
-			target.pointer_pressed(last_collided_at)
-
-func _button_released():
-	if target:
-		if target.has_signal("pointer_released"):
-			target.emit_signal("pointer_released", last_collided_at)
-		elif target.has_method("pointer_released"):
-			target.pointer_released(last_collided_at)
-
-		# unset target
-		target = null
-		last_collided_at = Vector3(0, 0, 0)
-
-func _on_button_pressed(p_button):
-	if p_button == active_button and enabled:
-		_button_pressed()
-
-func _on_button_release(p_button):
-	if p_button == active_button and target:
-		_button_released()
-
+# Called when the node enters the scene tree for the first time.
 func _ready():
+	# Do not initialise if in the editor
+	if Engine.editor_hint:
+		return
+
+	# Read the initial world-scale
 	ws = ARVRServer.world_scale
 
+	# If pointer-trigger is a button then subscribe to button signals
 	if active_button != Buttons.VR_ACTION:
 		# Get button press feedback from our parent (should be an ARVRController)
 		get_parent().connect("button_pressed", self, "_on_button_pressed")
 		get_parent().connect("button_release", self, "_on_button_release")
 
 	# init our state
-	set_y_offset(y_offset)
-	set_distance(distance)
-	set_collision_mask(collision_mask)
-	set_show_laser(show_laser)
-	set_collide_with_bodies(collide_with_bodies)
-	set_collide_with_areas(collide_with_areas)
-	set_enabled(enabled)
+	_update_y_offset()
+	_update_distance()
+	_update_collision_mask()
+	_update_show_laser()
+	_update_collide_with_bodies()
+	_update_collide_with_areas()
+	_update_set_enabled()
 
+
+# Called on each frame to update the pickup
 func _process(_delta):
-	if !is_inside_tree():
+	# Do not process if in the editor
+	if Engine.editor_hint or !is_inside_tree():
 		return
 
+	# If pointer-trigger is an action then check for action
 	if active_button == Buttons.VR_ACTION and action != "":
 		if Input.is_action_just_pressed(action):
 			_button_pressed()
 		elif !Input.is_action_pressed(action) and target:
 			_button_released()
 
-	var new_ws = ARVRServer.world_scale
+	# Handle world-scale changes
+	var new_ws := ARVRServer.world_scale
 	if (ws != new_ws):
 		ws = new_ws
-		set_y_offset(y_offset)
+		_update_y_offset()
 
 	if enabled and $RayCast.is_colliding():
 		var new_at = $RayCast.get_collision_point()
@@ -193,3 +181,130 @@ func _process(_delta):
 
 		last_target = null
 		$Target.visible = false
+
+
+# Set pointer enabled property
+func set_enabled(p_enabled : bool) -> void:
+	enabled = p_enabled
+
+	# this gets called before our scene is ready, we'll call this again in _ready to enable this
+	if is_inside_tree():
+		_update_set_enabled()
+
+
+# Set show-laser property
+func set_show_laser(p_show : bool) -> void:
+	show_laser = p_show
+	if is_inside_tree():
+		_update_show_laser()
+
+
+# Set pointer Y offset property
+func set_y_offset(p_offset : float) -> void:
+	y_offset = p_offset
+	if is_inside_tree():
+		_update_y_offset()
+
+
+# Set pointer distance property
+func set_distance(p_new_value : float) -> void:
+	distance = p_new_value
+	if is_inside_tree():
+		_update_distance()
+
+
+# Set pointer collision mask property
+func set_collision_mask(p_new_mask : int) -> void:
+	collision_mask = p_new_mask
+	if is_inside_tree():
+		_update_collision_mask()
+
+
+# Set pointer collide-with-bodies property
+func set_collide_with_bodies(p_new_value : bool) -> void:
+	collide_with_bodies = p_new_value
+	if is_inside_tree():
+		_update_collide_with_bodies()
+
+
+# Set pointer collide-with-areas property
+func set_collide_with_areas(p_new_value : bool) -> void:
+	collide_with_areas = p_new_value
+	if is_inside_tree():
+		_update_collide_with_areas()
+
+
+# Pointer enabled update handler
+func _update_set_enabled() -> void:
+	$Laser.visible = enabled and show_laser
+	$RayCast.enabled = enabled
+
+
+# Pointer show-laser update handler
+func _update_show_laser() -> void:
+	$Laser.visible = enabled and show_laser
+
+
+# Pointer Y offset update handler
+func _update_y_offset() -> void:
+	$Laser.translation.y = y_offset * ws
+	$RayCast.translation.y = y_offset * ws
+
+
+# Pointer distance update handler
+func _update_distance() -> void:
+	$Laser.mesh.size.z = distance
+	$Laser.translation.z = distance * -0.5
+	$RayCast.cast_to.z = -distance
+
+
+# Pointer collision mask update handler
+func _update_collision_mask() -> void:
+	$RayCast.collision_mask = collision_mask
+
+
+# Pointer collide-with-bodies update handler
+func _update_collide_with_bodies() -> void:
+	$RayCast.collide_with_bodies = collide_with_bodies
+
+
+# Pointer collide-with-areas update handler
+func _update_collide_with_areas() -> void:
+	$RayCast.collide_with_areas = collide_with_areas
+
+
+# Pointer-activation button pressed handler
+func _button_pressed() -> void:
+	if $RayCast.is_colliding():
+		target = $RayCast.get_collider()
+		last_collided_at = $RayCast.get_collision_point()
+
+		if target.has_signal("pointer_pressed"):
+			target.emit_signal("pointer_pressed", last_collided_at)
+		elif target.has_method("pointer_pressed"):
+			target.pointer_pressed(last_collided_at)
+
+
+# Pointer-activation button released handler
+func _button_released() -> void:
+	if target:
+		if target.has_signal("pointer_released"):
+			target.emit_signal("pointer_released", last_collided_at)
+		elif target.has_method("pointer_released"):
+			target.pointer_released(last_collided_at)
+
+		# unset target
+		target = null
+		last_collided_at = Vector3(0, 0, 0)
+
+
+# Button pressed handler
+func _on_button_pressed(p_button : int) -> void:
+	if p_button == active_button and enabled:
+		_button_pressed()
+
+
+# Button released handler
+func _on_button_release(p_button : int) -> void:
+	if p_button == active_button and target:
+		_button_released()
--- addons/godot-xr-tools/objects/viewport_2d_in_3d.gd
+++ addons/godot-xr-tools/objects/viewport_2d_in_3d.gd
@@ -1,93 +1,169 @@
 tool
 extends Spatial
 
+
+##
+## Viewport 2D in 3D
+##
+## @desc:
+##     This script manages a 2D scene rendered as a texture on a 3D quad.
+##
+##     Pointer and keyboard input are mapped into the 2D scene.
+##
+
+
 signal pointer_entered
 signal pointer_exited
 
-export var enabled = true setget set_enabled
-export var screen_size = Vector2(3.0, 2.0) setget set_screen_size
-export var viewport_size = Vector2(300.0, 200.0) setget set_viewport_size
-export var transparent = true setget set_transparent
-export var scene: PackedScene setget set_scene, get_scene
 
-# Need to replace this with proper solution once support for layer selection has been added
-export (int, LAYERS_3D_PHYSICS) var collision_layer = 15 setget set_collision_layer
+## Viewport enabled property
+export var enabled : bool = true setget set_enabled
 
-var is_ready = false
-var scene_node = null
+## Screen size property
+export var screen_size : Vector2 = Vector2(3.0, 2.0) setget set_screen_size
 
-func set_enabled(is_enabled: bool):
-	enabled = is_enabled
-	if is_ready:
-		$StaticBody/CollisionShape.disabled = !enabled
+## Viewport size property
+export var viewport_size : Vector2 = Vector2(300.0, 200.0) setget set_viewport_size
 
-func set_screen_size(new_size: Vector2):
-	screen_size = new_size
-	if is_ready:
-		$Screen.mesh.size = screen_size
-		$StaticBody.screen_size = screen_size
-		$StaticBody/CollisionShape.shape.extents = Vector3(screen_size.x * 0.5, screen_size.y * 0.5, 0.01)
+## Transparent property
+export var transparent : bool = true setget set_transparent
 
-func set_viewport_size(new_size: Vector2):
-	viewport_size = new_size
-	if is_ready:
-		$Viewport.size = new_size
-		$StaticBody.viewport_size = new_size
-		var material : SpatialMaterial = $Screen.get_surface_material(0)
-		material.albedo_texture = $Viewport.get_texture()
+## Scene property
+export var scene : PackedScene setget set_scene
 
-func set_transparent(new_transparent: bool):
-	transparent = new_transparent
-	if is_ready:
-		var material : SpatialMaterial = $Screen.get_surface_material(0)
-		material.flags_transparent = transparent
-		$Viewport.transparent_bg = transparent
+## Collision layer
+export (int, LAYERS_3D_PHYSICS) var collision_layer : int = 15 setget set_collision_layer
 
-func set_collision_layer(new_layer: int):
-	collision_layer = new_layer
-	if is_ready:
-		$StaticBody.collision_layer = collision_layer
 
-func set_scene(new_scene: PackedScene):
-	scene = new_scene
-	if is_ready:
-		# out with the old
-		if scene_node:
-			$Viewport.remove_child(scene_node)
-			scene_node.queue_free()
+var is_ready : bool = false
+var scene_node : Node
 
-		# in with the new
-		if scene:
-			scene_node = scene.instance()
-			$Viewport.add_child(scene_node)
 
-func get_scene():
-	return scene
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Do not initialize if in the editor
+	if Engine.editor_hint:
+		return
 
+	# apply properties
+	is_ready = true
+	_update_enabled()
+	_update_screen_size()
+	_update_viewport_size()
+	_update_collision_layer()
+	_update_scene()
+	_update_collision_layer()
+	_update_transparent()
+
+
+# Get the 2D scene instance
 func get_scene_instance():
 	return scene_node
 
+
+# Connect a 2D scene signal
 func connect_scene_signal(which, on, callback):
 	if scene_node:
 		scene_node.connect(which, on, callback)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	# apply properties
-	is_ready = true
-	set_enabled(enabled)
-	set_screen_size(screen_size)
-	set_viewport_size(viewport_size)
-	set_scene(scene)
-	set_collision_layer(collision_layer)
-	set_transparent(transparent)
-	set_process_input(true)
 
+# Handler for pointer entered
 func _on_pointer_entered():
 	emit_signal("pointer_entered")
 
+
+# Handler for pointer exited
 func _on_pointer_exited():
 	emit_signal("pointer_exited")
 
+
+# Handler for input eventsd
 func _input(event):
 	$Viewport.input(event)
+
+
+# Set enabled property
+func set_enabled(is_enabled: bool) -> void:
+	enabled = is_enabled
+	if is_ready:
+		_update_enabled()
+
+
+# Set screen size property
+func set_screen_size(new_size: Vector2) -> void:
+	screen_size = new_size
+	if is_ready:
+		_update_screen_size()
+
+
+# Set viewport size property
+func set_viewport_size(new_size: Vector2) -> void:
+	viewport_size = new_size
+	if is_ready:
+		_update_viewport_size()
+
+
+# Set transparent property
+func set_transparent(new_transparent: bool) -> void:
+	transparent = new_transparent
+	if is_ready:
+		_update_transparent()
+
+
+# Set scene property
+func set_scene(new_scene: PackedScene) -> void:
+	scene = new_scene
+	if is_ready:
+		_update_scene()
+
+
+# Set collision layer property
+func set_collision_layer(new_layer: int) -> void:
+	collision_layer = new_layer
+	if is_ready:
+		_update_collision_layer()
+
+
+# Enabled update handler
+func _update_enabled() -> void:
+	$StaticBody/CollisionShape.disabled = !enabled
+
+
+# Screen size update handler
+func _update_screen_size() -> void:
+	$Screen.mesh.size = screen_size
+	$StaticBody.screen_size = screen_size
+	$StaticBody/CollisionShape.shape.extents = Vector3(screen_size.x * 0.5, screen_size.y * 0.5, 0.01)
+
+
+# Viewport size update handler
+func _update_viewport_size() -> void:
+	$Viewport.size = viewport_size
+	$StaticBody.viewport_size = viewport_size
+	var material : SpatialMaterial = $Screen.get_surface_material(0)
+	material.albedo_texture = $Viewport.get_texture()
+
+
+# Transparent update handler
+func _update_transparent() -> void:
+	var material : SpatialMaterial = $Screen.get_surface_material(0)
+	material.flags_transparent = transparent
+	$Viewport.transparent_bg = transparent
+
+
+# Scene update handler
+func _update_scene() -> void:
+	# out with the old
+	if scene_node:
+		$Viewport.remove_child(scene_node)
+		scene_node.queue_free()
+
+	# in with the new
+	if scene:
+		scene_node = scene.instance()
+		$Viewport.add_child(scene_node)
+
+
+# Collision layer update handler
+func _update_collision_layer() -> void:
+	$StaticBody.collision_layer = collision_layer
