43469b5ab105acce5c2db68095040c8a9023e5a2
Malcolm Nixon <Malcolm.nixon@gmail.com>
2022-10-13 22:34:33 -0400

    Sync of 3.x changes into 4.x Added cutout shader parameter to loading screen progress shader.
    Merged object-scenes and inherited-scenes cleanup from 3.x

--- addons/godot-xr-tools/functions/function_pointer.gd
+++ addons/godot-xr-tools/functions/function_pointer.gd
@@ -3,119 +3,73 @@ class_name XRToolsFunctionPointer
 extends Node3D
 @icon("res://addons/godot-xr-tools/editor/icons/function.svg")
 
-@export var enabled = true:
-	set(new_value):
-		enabled = new_value
-		
-		# this gets called before our scene is ready, we'll call this again in _ready to enable this
-		if is_inside_tree():
-			_update_set_enabled()
 
-func _update_set_enabled():
-	$Laser.visible = enabled and show_laser
-	$RayCast.enabled = enabled
-
-@export var show_laser = true:
-	set(new_value):
-		show_laser = new_value
-		if is_inside_tree():
-			_update_show_laser();
-
-func _update_show_laser():
-	$Laser.visible = enabled and show_laser
-
-@export var show_target = false
-@export var active_button_action = "trigger_click"
-@export var y_offset = -0.05:
-	set(new_value):
-		y_offset = new_value
-		if is_inside_tree():
-			_update_y_offset()
-
-func _update_y_offset():
-	$Laser.position.y = y_offset * ws
-	$RayCast.position.y = y_offset * ws
-
-@export var distance = 10:
-	set(new_value):
-		distance = new_value
-		if is_inside_tree():
-			_update_distance();
-
-func _update_distance():
-	$Laser.mesh.size.z = distance
-	$Laser.position.z = distance * -0.5
-	$RayCast.target_position.z = -distance
-
-@export_flags_3d_physics  var collision_mask = 15:
-	set(new_value):
-		collision_mask = new_value
-		if is_inside_tree():
-			_update_collision_mask()
-
-func _update_collision_mask():
-	$RayCast.collision_mask = collision_mask
-
-@export var collide_with_bodies = true:
-	set(new_value):
-		collide_with_bodies = new_value
-		if is_inside_tree():
-			_update_collide_with_bodies()
-
-func _update_collide_with_bodies():
-	$RayCast.collide_with_bodies = collide_with_bodies
+##
+## Pointer Function Script
+##
+## @desc:
+##     This script implements a pointer function for a players controller. The
+##     pointer supports sending signals to XRToolsInteractableArea or 
+##     XRToolsInteractableBody objects.
+##
+##     The following signals are sent to these objects:
+##      - pointer_pressed(at) with the pointer location
+##      - pointer_released(at) with the pointer location
+##      - pointer_moved(from, to) with the pointer movement
+##      - pointer_entered()
+##      - pointer_exited()
+##
 
 
-@export var collide_with_areas = false:
-	set(new_value):
-		collide_with_areas = new_value
-		if is_inside_tree():
-			_update_collide_with_areas()
+## Pointer enabled property
+@export var enabled : bool = true: set = set_enabled
 
-func _update_collide_with_areas():
-	$RayCast.collide_with_areas = collide_with_areas
+## Show laser property
+@export var show_laser : bool = true: set = set_show_laser
 
-var target = null
-var last_target = null
-var last_collided_at = Vector3(0, 0, 0)
+## Show laser target
+@export var show_target : bool = false
 
-var ws = 1.0
+## Y Offset for pointer
+@export var y_offset : float = -0.05: set = set_y_offset
 
-func _button_pressed():
-	if $RayCast.is_colliding():
-		target = $RayCast.get_collider()
-		last_collided_at = $RayCast.get_collision_point()
-		
-		if target.has_signal("pointer_pressed"):
-			target.emit_signal("pointer_pressed", last_collided_at)
-		elif target.has_method("pointer_pressed"):
-			target.pointer_pressed(last_collided_at)
+## Pointer distance
+@export var distance : float = 10: set = set_distance
 
-func _button_released():
-	if target:
-		if target.has_signal("pointer_released"):
-			target.emit_signal("pointer_released", last_collided_at)
-		elif target.has_method("pointer_released"):
-			target.pointer_released(last_collided_at)
-		
-		# unset target
-		target = null
-		last_collided_at = Vector3(0, 0, 0)
+## Pointer collision mask
+@export_flags_3d_physics var collision_mask : int = 15: set = set_collision_mask
 
-func _on_button_pressed(p_button):
-	if p_button == active_button_action and enabled:
-		_button_pressed()
+## Enable pointer collision with bodies
+@export var collide_with_bodies : bool = true: set = set_collide_with_bodies
 
-func _on_button_released(p_button):
-	if p_button == active_button_action and target:
-		_button_released()
+## Enable pointer collision with areas
+@export var collide_with_areas : bool = false: set = set_collide_with_areas
 
+## Active button action
+@export var active_button_action : String = "trigger_click"
+
+
+# Current target
+var target : Node3D
+
+# Last target
+var last_target : Node3D
+
+# Last collision point
+var last_collided_at : Vector3 = Vector3.ZERO
+
+# World scale
+var ws : float = 1.0
+
+
+# Called when the node enters the scene tree for the first time.
 func _ready():
-	ws = XRServer.world_scale
-
 	# Do not initialise if in the editor
 	if Engine.is_editor_hint():
 		return
+
+	# Read the initial world-scale
+	ws = XRServer.world_scale
 
 	# Get button press feedback from our parent (should be an XRController3D)
 	get_parent().connect("button_pressed", _on_button_pressed)
@@ -130,15 +84,16 @@ func _ready():
 	_update_collide_with_areas()
 	_update_set_enabled()
 
-func _process(delta):
-	# Do not run physics if in the editor
-	if Engine.is_editor_hint():
+
+# Called on each frame to update the pickup
+func _process(_delta):
+	# Do not process if in the editor
+	if Engine.is_editor_hint() or !is_inside_tree():
 		return
 
-	if !is_inside_tree():
-		return
 
-	var new_ws = XRServer.world_scale
+	# Handle world-scale changes
+	var new_ws := XRServer.world_scale
 	if (ws != new_ws):
 		ws = new_ws
 		_update_y_offset()
@@ -195,3 +150,130 @@ func _process(delta):
 
 		last_target = null
 		$Target.visible = false
+
+
+# Set pointer enabled property
+func set_enabled(p_enabled : bool) -> void:
+	enabled = p_enabled
+
+	# this gets called before our scene is ready, we'll call this again in _ready to enable this
+	if is_inside_tree():
+		_update_set_enabled()
+
+
+# Set show-laser property
+func set_show_laser(p_show : bool) -> void:
+	show_laser = p_show
+	if is_inside_tree():
+		_update_show_laser()
+
+
+# Set pointer Y offset property
+func set_y_offset(p_offset : float) -> void:
+	y_offset = p_offset
+	if is_inside_tree():
+		_update_y_offset()
+
+
+# Set pointer distance property
+func set_distance(p_new_value : float) -> void:
+	distance = p_new_value
+	if is_inside_tree():
+		_update_distance()
+
+
+# Set pointer collision mask property
+func set_collision_mask(p_new_mask : int) -> void:
+	collision_mask = p_new_mask
+	if is_inside_tree():
+		_update_collision_mask()
+
+
+# Set pointer collide-with-bodies property
+func set_collide_with_bodies(p_new_value : bool) -> void:
+	collide_with_bodies = p_new_value
+	if is_inside_tree():
+		_update_collide_with_bodies()
+
+
+# Set pointer collide-with-areas property
+func set_collide_with_areas(p_new_value : bool) -> void:
+	collide_with_areas = p_new_value
+	if is_inside_tree():
+		_update_collide_with_areas()
+
+
+# Pointer enabled update handler
+func _update_set_enabled() -> void:
+	$Laser.visible = enabled and show_laser
+	$RayCast.enabled = enabled
+
+
+# Pointer show-laser update handler
+func _update_show_laser() -> void:
+	$Laser.visible = enabled and show_laser
+
+
+# Pointer Y offset update handler
+func _update_y_offset() -> void:
+	$Laser.position.y = y_offset * ws
+	$RayCast.position.y = y_offset * ws
+
+
+# Pointer distance update handler
+func _update_distance() -> void:
+	$Laser.mesh.size.z = distance
+	$Laser.position.z = distance * -0.5
+	$RayCast.target_position.z = -distance
+
+
+# Pointer collision mask update handler
+func _update_collision_mask() -> void:
+	$RayCast.collision_mask = collision_mask
+
+
+# Pointer collide-with-bodies update handler
+func _update_collide_with_bodies() -> void:
+	$RayCast.collide_with_bodies = collide_with_bodies
+
+
+# Pointer collide-with-areas update handler
+func _update_collide_with_areas() -> void:
+	$RayCast.collide_with_areas = collide_with_areas
+
+
+# Pointer-activation button pressed handler
+func _button_pressed() -> void:
+	if $RayCast.is_colliding():
+		target = $RayCast.get_collider()
+		last_collided_at = $RayCast.get_collision_point()
+
+		if target.has_signal("pointer_pressed"):
+			target.emit_signal("pointer_pressed", last_collided_at)
+		elif target.has_method("pointer_pressed"):
+			target.pointer_pressed(last_collided_at)
+
+
+# Pointer-activation button released handler
+func _button_released() -> void:
+	if target:
+		if target.has_signal("pointer_released"):
+			target.emit_signal("pointer_released", last_collided_at)
+		elif target.has_method("pointer_released"):
+			target.pointer_released(last_collided_at)
+
+		# unset target
+		target = null
+		last_collided_at = Vector3(0, 0, 0)
+
+
+# Button pressed handler
+func _on_button_pressed(p_button : String) -> void:
+	if p_button == active_button_action and enabled:
+		_button_pressed()
+
+
+# Button released handler
+func _on_button_released(p_button : String) -> void:
+	if p_button == active_button_action and target:
+		_button_released()
