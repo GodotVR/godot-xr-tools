cf86ee007e387aeece2aef31f2bae5c2304414b5
Malcolm Nixon <Malcolm.nixon@gmail.com>
2022-10-18 07:18:21 -0400

    Changed XRToolsMovementTurn to XRToolsFunctionTurn Modified Teleport demo to have no XRToolsPlayerBody

--- addons/godot-xr-tools/functions/function_turn.gd
+++ addons/godot-xr-tools/functions/function_turn.gd
@@ -1,22 +1,18 @@
 tool
-class_name XRToolsMovementTurn
-extends XRToolsMovementProvider
+class_name XRToolsFunctionTurn, "res://addons/godot-xr-tools/editor/icons/function.svg"
+extends Node
 
 ##
 ## Movement Provider for Turning
 ##
 ## @desc:
-##     This script provides turning support for the player. This script works
-##     with the PlayerBody attached to the players ARVROrigin.
+##     This script provides turning support for the player.
 ##
 ##     The following types of turning are supported:
 ##      - Snap turning
 ##      - Smooth turning
 ##
 
-
-## Movement provider order
-export var order : int = 5
 
 ## Use smooth rotation (may cause motion sickness)
 export var smooth_rotation : bool = false
@@ -38,11 +34,17 @@ var _turn_step : float = 0.0
 # Controller node
 onready var _controller : ARVRController = get_parent()
 
+# Origin node
+onready var _origin : ARVROrigin = ARVRHelpers.get_arvr_origin(self)
+
+# Camea node
+onready var _camera : ARVRCamera = ARVRHelpers.get_arvr_camera(self)
+
 
 # Perform jump movement
-func physics_movement(delta: float, player_body: XRToolsPlayerBody, _disabled: bool):
-	# Skip if the controller isn't active
-	if !_controller.get_is_active():
+func _process(delta: float):
+	# Skip processing in editor or if the controller isn't active
+	if Engine.editor_hint or !_controller.get_is_active():
 		return
 
 	# Read the left/right joystick axis
@@ -54,7 +56,7 @@ func physics_movement(delta: float, player_body: XRToolsPlayerBody, _disabled: b
 
 	# Handle smooth rotation
 	if smooth_rotation:
-		_rotate_player(player_body, smooth_turn_speed * delta * left_right)
+		_rotate_player(smooth_turn_speed * delta * left_right)
 		return
 
 	# Update the next turn-step delay
@@ -64,27 +66,37 @@ func physics_movement(delta: float, player_body: XRToolsPlayerBody, _disabled: b
 
 	# Turn one step in the requested direction
 	_turn_step = step_turn_delay
-	_rotate_player(player_body, deg2rad(step_turn_angle) * sign(left_right))
+	_rotate_player(deg2rad(step_turn_angle) * sign(left_right))
 
 
 # Rotate the origin node around the camera
-func _rotate_player(player_body: XRToolsPlayerBody, angle: float):
+func _rotate_player(angle: float):
 	var t1 := Transform()
 	var t2 := Transform()
 	var rot := Transform()
 
-	t1.origin = -player_body.camera_node.transform.origin
-	t2.origin = player_body.camera_node.transform.origin
+	t1.origin = -_camera.transform.origin
+	t2.origin = _camera.transform.origin
 	rot = rot.rotated(Vector3(0.0, -1.0, 0.0), angle)
-	player_body.origin_node.transform = (player_body.origin_node.transform * t2 * rot * t1).orthonormalized()
+	_origin.transform = (_origin.transform * t2 * rot * t1).orthonormalized()
 
 
 # This method verifies the movement provider has a valid configuration.
-func _get_configuration_warning():
+func _get_configuration_warnings():
 	# Check the controller node
 	var test_controller = get_parent()
 	if !test_controller or !test_controller is ARVRController:
 		return "Unable to find ARVR Controller node"
 
+	# Check the origin node
+	var test_origin = ARVRHelpers.get_arvr_origin(self)
+	if !test_origin:
+		return "Unable to find ARVR Origin node"
+
+	# Check the camera node
+	var test_camera = ARVRHelpers.get_arvr_camera(self)
+	if !test_camera:
+		return "Unable to find ARVR Camera node"
+
 	# Call base class
-	return ._get_configuration_warning()
+	return ._get_configuration_warnings()
