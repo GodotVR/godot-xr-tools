9f63ca19c46a6e8503e0685babc96ba5fa909c8a
Malcolm Nixon <Malcolm.nixon@gmail.com>
2022-10-30 19:36:35 -0400

    Added support for crouch-toggle to XRToolsMovementCrouch. (#234)
    Added crouching to the basic movement demo - including updated instructions with filtering for clarity.

--- addons/godot-xr-tools/functions/movement_crouch.gd
+++ addons/godot-xr-tools/functions/movement_crouch.gd
@@ -2,16 +2,20 @@ tool
 class_name XRToolsMovementCrouch
 extends XRToolsMovementProvider
 
+## XR Tools Movement Provider for Crouching
 ##
-## Movement Provider for Crouching
+## This script works with the [XRToolsPlayerBody] attached to the players 
+## [ARVROrigin].
 ##
-## @desc:
-##     This script works with the PlayerBody attached to the players ARVROrigin.
-##
+## While the player presses the crounch button, the height is overridden to
+## the specified crouch height.
 
-##     When the player presses the selected button, the height is overridden
-##     to the crouch height
-##
+
+## Enumeration of crouching modes
+enum CrouchType {
+	HOLD_TO_CROUCH,	## Hold button to crouch
+	TOGGLE_CROUCH,	## Toggle crouching on button press
+}
 
 
 ## Movement provider order
@@ -23,9 +27,15 @@ export var crouch_height : float = 1.0
 ## Crouch button
 export (XRTools.Buttons) var crouch_button : int = XRTools.Buttons.VR_PAD
 
+## Type of crouching
+export (CrouchType) var crouch_type : int = CrouchType.HOLD_TO_CROUCH
+
 
 ## Crouching flag
 var _crouching : bool = false
+
+## Crouch button down state
+var _crouch_button_down : bool = false
 
 
 # Controller node
@@ -38,17 +48,30 @@ func physics_movement(_delta: float, player_body: XRToolsPlayerBody, _disabled: 
 	if !_controller.get_is_active():
 		return
 
-	# Check for crouching change
-	var crouching := _controller.is_button_pressed(crouch_button) != 0
-	if crouching == _crouching:
-		return
+	# Detect crouch button down and pressed states
+	var crouch_button_down := _controller.is_button_pressed(crouch_button) != 0
+	var crouch_button_pressed := crouch_button_down and !_crouch_button_down
+	_crouch_button_down = crouch_button_down
+
+	# Calculate new crouching state
+	var crouching := _crouching
+	match crouch_type:
+		CrouchType.HOLD_TO_CROUCH:
+			# Crouch when button down
+			crouching = crouch_button_down
+
+		CrouchType.TOGGLE_CROUCH:
+			# Toggle when button pressed
+			if crouch_button_pressed:
+				crouching = !crouching
 
 	# Update crouching state
-	_crouching = crouching
-	if crouching:
-		player_body.override_player_height(self, crouch_height)
-	else:
-		player_body.override_player_height(self)
+	if crouching != _crouching:
+		_crouching = crouching
+		if crouching:
+			player_body.override_player_height(self, crouch_height)
+		else:
+			player_body.override_player_height(self)
 
 
 # This method verifies the movement provider has a valid configuration.
