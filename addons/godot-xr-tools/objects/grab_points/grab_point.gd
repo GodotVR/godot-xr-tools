80c3658df7605ba2968a372de9a22c5ba3c6d2b1
Malcolm Nixon <Malcolm.nixon@gmail.com>
2022-10-30 13:35:08 -0400

    Added configurable grab-points
    Moved standard hand offset [+/- 0.03, -0.05, 0.15] from all the scenes into the hand assets. Added grab points to the interactable demo objects. Implemented grab-points in XRToolsPickable.
    Cleanup of XRToolsPickable script and scene to remove obsolete parameters. Modified grab cube to use precise grab.
    
    Modified XRToolsGrabPointHand to load the new hand model. Tuned hammer grab-point.
    
    Added hand-pose to hand grab-points
    
    Modified all grab_point objects to extend from Position3D to help aligning in the editor. Modified default_env.tres to give some sky contribution. Set default_env.tres to be the default editor environment to better illuminate objects being edited. Added teacups to pickable demo to demonstrate multiple snap grab-points.
    
    Added get_controller() to XRToolsFunctionPickup Modified XRToolsGrabPointHand to use get_controller() rather than assuming get_parent() gets the controller

---
+++ addons/godot-xr-tools/objects/grab_points/grab_point.gd
@@ -0,0 +1,18 @@
+class_name XRToolsGrabPoint
+extends Position3D
+
+
+## XR Tools Grab Point Base Script
+##
+## This script is the base for all grab points. Pickable object extending from
+## [XRToolsPickable] can have numerous grab points to control where the object
+## is grabbed from.
+
+
+## If true, the grab point is enabled for grabbing
+export var enabled : bool = true
+
+
+## Test if a grabber can grab by this grab-point
+func can_grab(_grabber : Node) -> bool:
+	return enabled
---
+++ addons/godot-xr-tools/objects/grab_points/grab_point_snap.gd
@@ -0,0 +1,45 @@
+tool
+class_name XRToolsGrabPointSnap
+extends XRToolsGrabPoint
+
+
+## XR Tools Grab Point Snap Script
+##
+## This script allows specifying a grab point for snap zones. It supports
+## group-filters if different points are required for different snap zones.
+
+
+## Require grab-by to be in the specified group
+export var require_group : String = ""
+
+## Deny grab-by if in the specified group
+export var exclude_group : String = ""
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Add a Position3D child to help editor visibility
+	if Engine.editor_hint:
+		add_child(Position3D.new())
+
+
+## Test if a grabber can grab by this grab-point
+func can_grab(_grabber : Node) -> bool:
+	# Skip if not enabled
+	if not enabled:
+		return false
+
+	# Ensure the pickup is valid
+	if not is_instance_valid(_grabber):
+		return false
+
+	# Refuse if the grabber is not in the required group
+	if not require_group.empty() and not _grabber.is_in_group(require_group):
+		return false
+
+	# Refuse if the grabber is in the excluded group
+	if not exclude_group.empty() and _grabber.is_in_group(exclude_group):
+		return false
+
+	# Allow the grab
+	return true
