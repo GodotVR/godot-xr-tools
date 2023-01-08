915a44891b5ff9f24f462d1115c52f8179ea442e
pietru2004 <51274570+pietru2004@users.noreply.github.com>
2023-01-08 21:24:38 +0100

    Make Demos show fancier

--- scenes/main_menu/main_menu_level.gd
+++ scenes/main_menu/main_menu_level.gd
@@ -2,7 +2,12 @@
 extends "../scene_base.gd"
 
 func _update_demo_positions() -> void:
-	var count = $Demos.get_child_count()
+	var count = 0#$Demos.get_child_count()
+	var vis_chd := []
+	for _c in $Demos.get_children():
+		if _c.visible:
+			count+=1
+			vis_chd.append(_c)
 	if count > 1:
 		var angle = 2.0 * PI / count
 		for i in count:
@@ -10,12 +15,15 @@ func _update_demo_positions() -> void:
 			t.origin = Vector3(0.0, 0.0, -10.0)
 			t = t.rotated(Vector3.UP, angle * i)
 
-			$Demos.get_child(i).transform = t
+			vis_chd[i].transform = t
 
 
 func _ready():
 	super._ready()
 	_update_demo_positions()
+	
+	for _c in $Demos.get_children():
+		_c.connect("visibility_changed",_update_demo_positions)
 
 
 func _on_Demos_child_entered_tree(_node):
