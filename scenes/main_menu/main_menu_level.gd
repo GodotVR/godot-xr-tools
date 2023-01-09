481103d97df0c48f06cf0ae63dcc5c769a6b3191
pietru2004 <51274570+pietru2004@users.noreply.github.com>
2023-01-09 17:34:29 +0100

    Update main_menu_level.gd

--- scenes/main_menu/main_menu_level.gd
+++ scenes/main_menu/main_menu_level.gd
@@ -2,13 +2,13 @@
 extends "../scene_base.gd"
 
 func _update_demo_positions() -> void:
-	var count = 0#$Demos.get_child_count()
-	var vis_chd := []
-	for _tp in $Demos.get_children():
-		_tp.active=_tp.visible
-		if _tp.visible:
+	var count = 0
+	var visible_children := []
+	for teleporter in $Demos.get_children():
+		teleporter.active=teleporter.visible
+		if teleporter.visible:
 			count+=1
-			vis_chd.append(_tp)
+			visible_children.append(teleporter)
 	if count > 1:
 		var angle = 2.0 * PI / count
 		for i in count:
@@ -16,15 +16,15 @@ func _update_demo_positions() -> void:
 			t.origin = Vector3(0.0, 0.0, -10.0)
 			t = t.rotated(Vector3.UP, angle * i)
 
-			vis_chd[i].transform = t
+			visible_children[i].transform = t
 
 
 func _ready():
 	super._ready()
 	_update_demo_positions()
 	
-	for _c in $Demos.get_children():
-		_c.connect("visibility_changed",_update_demo_positions)
+	for teleporter in $Demos.get_children():
+		teleporter.connect("visibility_changed",_update_demo_positions)
 
 
 func _on_Demos_child_entered_tree(_node):
