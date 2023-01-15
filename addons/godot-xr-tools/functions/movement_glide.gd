69e62d47e9cd03e31ac28e223c822e963fb53d75
carlos padial <carlos@asturnazari.com>
2023-01-15 01:12:47 +0100

    Flapping wings. Add vertical impulse when flapping arms (#322)
    * add vertical impulse when flapping arms
    
    * adjust wings force value
    
    * add flapping signal emit and rearm wings logic
    
    * add rearm distance exported variable
    
    * gdslint fixes and missing line
    
    * updated instructions board
    
    * contributors changes

--- addons/godot-xr-tools/functions/movement_glide.gd
+++ addons/godot-xr-tools/functions/movement_glide.gd
@@ -25,6 +25,8 @@ signal player_glide_start
 ## Signal invoked when the player ends gliding
 signal player_glide_end
 
+## Signal invoked when the player flaps
+signal player_flapped
 
 ## Movement provider order
 export var order : int = 35
@@ -53,6 +55,25 @@ export var turn_with_roll : bool = false
 ## Smooth turn speed in radians per second
 export var roll_turn_speed : float = 1
 
+## add vertical impulse by flapping controllers
+export var wings_impulse : bool = false
+
+## minimum velocity for flapping
+export var flap_min_speed : float = 0.3
+
+## flapping force multiplier
+export var wings_force : float = 1.0
+
+## minimum distance from controllers to ARVRCamera to rearm flaps
+## if set to 0, you need to reach head level with hands to rearm flaps
+export var rearm_distance_offset : float = 0.2
+
+## flap activated (when both controllers are near the ARVRCamera height)
+var flap_armed : bool = false
+
+## last controllers position to calculate flapping velocity
+var last_local_left_position : Vector3
+var last_local_right_position : Vector3
 
 # Left controller
 onready var _left_controller := ARVRHelpers.get_left_controller(self)
@@ -60,6 +81,8 @@ onready var _left_controller := ARVRHelpers.get_left_controller(self)
 # Right controller
 onready var _right_controller := ARVRHelpers.get_right_controller(self)
 
+# ARVRCamera
+onready var _camera_node := ARVRHelpers.get_arvr_camera(self)
 
 # Add support for is_class on XRTools classes
 func is_class(name : String) -> bool:
@@ -74,15 +97,65 @@ func physics_movement(delta: float, player_body: XRToolsPlayerBody, disabled: bo
 		_set_gliding(false)
 		return
 
-	# If on the ground, or not falling, then not gliding
-	var vertical_velocity := player_body.velocity.dot(player_body.up_gravity_vector)
-	if player_body.on_ground || vertical_velocity >= glide_min_fall_speed:
+	# If on the ground, then not gliding
+	if player_body.on_ground:
 		_set_gliding(false)
 		return
 
 	# Get the controller left and right global horizontal positions
 	var left_position := _left_controller.global_transform.origin
 	var right_position := _right_controller.global_transform.origin
+
+	#set default wings impulse to zero
+	var wings_impulse_velocity := 0.0
+	# if wings impulse is active, calculate flapping impulse
+	if wings_impulse:
+		# get head position
+		var camera_position := _camera_node.global_transform.origin
+		# check controllers position relative to head
+		var left_hand_over_head = camera_position.y < left_position.y + rearm_distance_offset
+		var right_hand_over_head = camera_position.y < right_position.y + rearm_distance_offset
+		if left_hand_over_head && right_hand_over_head:
+			flap_armed = true
+
+		if flap_armed:
+			# get controller local positions
+			var local_left_position := _left_controller.transform.origin
+			var local_right_position := _right_controller.transform.origin
+
+			# store last frame controller positions for the first step
+			if !last_local_left_position:
+				last_local_left_position = local_left_position
+			if !last_local_right_position:
+				last_local_right_position = local_right_position
+
+			# calculate controllers velocity only when flapping downwards
+			var left_wing_velocity = 0.0
+			var right_wing_velocity = 0.0
+			if local_left_position.y < last_local_left_position.y:
+					left_wing_velocity = local_left_position.distance_to(last_local_left_position) / delta
+			if local_right_position.y < last_local_right_position.y:
+					right_wing_velocity = local_right_position.distance_to(last_local_right_position) / delta
+
+			# calculate wings impulse
+			if left_wing_velocity > flap_min_speed && right_wing_velocity > flap_min_speed:
+				wings_impulse_velocity = (left_wing_velocity + right_wing_velocity) / 2
+				wings_impulse_velocity = wings_impulse_velocity * wings_force * delta * 50
+				emit_signal("player_flapped")
+				flap_armed = false
+
+			# store controller position for next frame
+			last_local_left_position = local_left_position
+			last_local_right_position = local_right_position
+
+	# If not falling, then not gliding
+	var vertical_velocity := player_body.velocity.dot(player_body.up_gravity_vector)
+	vertical_velocity += wings_impulse_velocity
+	if vertical_velocity >= glide_min_fall_speed && wings_impulse_velocity == 0.0:
+		_set_gliding(false)
+		return
+
+	# calculate global left to right controller vector
 	var left_to_right := right_position - left_position
 
 	if turn_with_roll:
