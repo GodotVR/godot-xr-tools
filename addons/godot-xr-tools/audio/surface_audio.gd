985c74479440420d97e6298021cc51ca317f5ac9
Miodrag Sejic <56046022+DigitalN8m4r3@users.noreply.github.com>
2023-02-17 14:00:27 +0100

    conversion of footstep system from 3x to 4x (#368)
    * conversion of footstep system from 3x to 4x - broken
    
    this is the first attempt at converting the footstep system, currently it is broken and will need some work to get working
    
    * added Default Surface Audio
    
    this brings the godot4 version on par with the godot3 version
    
    still issues remain, right now it plays only the default footstep audio and that is
    thx to Haledire from the godot4 discord channel who pointed me to the right conversion and how to use the bind
    
    * added actual footstep sounds
    
    this pr fixes the audio not playing when walking on surface
    + adds the sounds provided by @Malcolmnixon
    License: CC0 Public Domain
    
    - obsolete Debug audio files
    
    * replaced mud footstep audio

---
+++ addons/godot-xr-tools/audio/surface_audio.gd
@@ -0,0 +1,30 @@
+@tool
+@icon("res://addons/godot-xr-tools/editor/icons/foot.svg")
+class_name XRToolsSurfaceAudio
+extends Node
+
+
+## XRTools Surface Audio Node
+##
+## This node is attached as a child of a StaticObject to give it a surface
+## audio type. This will cause the XRToolsMovementFootStep to play the correct
+## foot-step sounds when walking on the object.
+
+
+## XRToolsSurfaceAudioType to associate with this surface
+@export var surface_audio_type : XRToolsSurfaceAudioType
+
+
+# Add support for is_class on XRTools classes
+func is_xr_class(name : String) -> bool:
+	return name == "XRToolsSurfaceAudio"
+
+
+# This method checks for configuration issues.
+func _get_configuration_warning():
+	# Verify the camera
+	if !surface_audio_type:
+		return "Surface audio type not specified"
+
+	# No configuration issues detected
+	return ""
