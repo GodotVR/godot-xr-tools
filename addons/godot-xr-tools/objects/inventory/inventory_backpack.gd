844e6f7825bb3dfdb57e9f8d362c7194c80dff4e
D@re <56046022+DigitalN8m4r3@users.noreply.github.com>
2023-02-28 02:05:39 +0100

    updated with optional return to player
    this adds a optional bool
    to return to player after wait time is passed, the backpack will then return from wherever it was previously dropped
    (currently this introduces some phyiscs issues, the backpack floats sometimes around like possesed)
    
    thx to malcolm who helped clean up some loose ends

--- addons/godot-xr-tools/objects/inventory/inventory_backpack.gd
+++ addons/godot-xr-tools/objects/inventory/inventory_backpack.gd
@@ -3,69 +3,29 @@ class_name XRToolsInventoryBackpack, "res://addons/godot-xr-tools/editor/icons/b
 extends Node
 
 
+export var return_to_player : bool = false
+export var wait_time = 3
 export (PackedScene) var backpack_scene
-export var backpack_path := @""
-onready var backpack := get_node(backpack_path) as XRToolsPickable
-var left_pickup = null
-var right_pickup = null
-
+export var backpack_path: NodePath
+onready var backpack: XRToolsPickable = get_node(backpack_path)
+# Array of all snap-zones in the backpack
+var _backpack_snap_zones := []
+var timer
 
 func _ready():
-	left_pickup = get_parent().get_node("LeftHand/FunctionPickup")
-	right_pickup = get_parent().get_node("RightHand/FunctionPickup")
-	left_pickup.connect("has_picked_up", self, "_on_left_pickup")
-	right_pickup.connect("has_picked_up", self, "_on_right_pickup")
+	timer = get_node("Timer")
+	timer.connect("timeout", self, "_on_Timer_timeout")
 
-	# if scene has a backpack, set the weapon snap zones to require
-	# a group that does not exist so player can just pick up
-	# a backpack without triggering snap zones
-	if backpack != null:
-		var inner_pack = backpack.get_node("In")
-		var outer_pack = backpack.get_node("Out")
-		var inner_snaps = inner_pack.get_children()
-		var outer_snaps = outer_pack.get_children()
-		for snap in inner_snaps:
-			snap.grab_require = "disabled"
-			#snap.connect("has_picked_up", self, "_on_snap_zone_picked_up_object")
-		for snap in outer_snaps:
-			snap.grab_require = "disabled"
-			#snap.connect("has_picked_up", self, "_on_snap_zone_picked_up_object")
+	# Listen to when this backpack is picked up or dropped
+	backpack.connect("picked_up", self, "_on_backpack_picked_up")
+	backpack.connect("dropped", self, "_on_backpack_dropped")
 
+	# Get all the snap-zones
+	_backpack_snap_zones = XRTools.find_children(backpack, "*", "XRToolsSnapZone")
 
-func _on_left_pickup(object):	
-	var inventory = check_inventory_scene(object)
-
-	if inventory == null:
-		return
-
-	if inventory == backpack_scene:
-		var inner_pack = object.get_node("In")
-		var outer_pack = object.get_node("Out")
-		var inner_snaps = inner_pack.get_children()
-		var outer_snaps = outer_pack.get_children()
-		for snap in inner_snaps:
-			snap.grab_require = ""
-		for snap in outer_snaps:
-			snap.grab_require = ""
-		return
-
-
-func _on_right_pickup(object):
-	var inventory = check_inventory_scene(object)
-	
-	if inventory == null:
-		return
-	
-	if inventory == backpack_scene:
-		var inner_pack = object.get_node("In")
-		var outer_pack = object.get_node("Out")
-		var inner_snaps = inner_pack.get_children()
-		var outer_snaps = outer_pack.get_children()
-		for snap in inner_snaps:
-			snap.grab_require = ""
-		for snap in outer_snaps:
-			snap.grab_require = ""
-		return
+	# Disable all snap-zones in this backpack
+	for snap in _backpack_snap_zones :
+		snap.enabled = false
 
 
 # compare object to weapon .tscns used in game to determine which it is
@@ -74,29 +34,45 @@ func check_inventory_scene(object):
 		return backpack_scene
 	return null
 
+func _on_backpack_picked_up(_pickable):
+	# Enable all snap-zones when picked up by a hand/controller
+	if backpack.get_picked_up_by_controller():
+		for snap in _backpack_snap_zones:
+			snap.enabled = true
 
-# If backpack dropped, freeze slots so player
-# cannot accidentally grab weapons from slots while picking up backpack
-func _on_SnapBackpack_dropped(pickable):
-	var inner_pack = pickable.get_node("In")
-	var outer_pack = pickable.get_node("Out")
-	var inner_snaps = inner_pack.get_children()
-	var outer_snaps = outer_pack.get_children()
-	for snap in inner_snaps:
-		snap.grab_require = "none"
+func _on_backpack_dropped(_pickable):
+	# Disable all snap-zones when the backpack is dropped
+	for snap in _backpack_snap_zones :
+		snap.enabled = false
 
-	for snap in outer_snaps:
-		snap.grab_require = "none"
+	if !return_to_player:
+		# example code to automatically return backpack
+		# to player holster when dropped
+			if _pickable.picked_up_by == null and _pickable.is_picked_up() == false:
+				var shoulder_holster = get_tree().get_nodes_in_group("ShoulderHolster")
 
-	# example code to automatically return backpack
-	# to player holster when dropped
-	if pickable.picked_up_by == null and pickable.is_picked_up() == false:
-		var shoulder_holster = get_tree().get_nodes_in_group("ShoulderHolster")
+				if shoulder_holster == null:
+					return
 
-		if shoulder_holster == null:
-			return
+				for holster in shoulder_holster:
+				#put on empty shoulder slot if one is available
+					if holster.picked_up_object == null:
+						holster._pick_up_object(backpack)
+	if return_to_player:
+		# example code to automatically return backpack
+		# to player holster when dropped
+		if _pickable.picked_up_by == null and _pickable.is_picked_up() == false:
+			timer.start(wait_time)
 
-		for holster in shoulder_holster:
-		#put on empty shoulder slot if one is available
-			if holster.picked_up_object == null:
-				holster._pick_up_object(backpack)
+
+func _on_Timer_timeout():
+	var shoulder_holster = get_tree().get_nodes_in_group("ShoulderHolster")
+
+	if shoulder_holster == null:
+		return
+
+	for holster in shoulder_holster:
+	#put on empty shoulder slot if one is available
+		if holster.picked_up_object == null:
+			holster._pick_up_object(backpack)
+			break
