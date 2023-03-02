045c668ce8ca9677249e3165d904897956bcfef0
D@re <56046022+DigitalN8m4r3@users.noreply.github.com>
2023-03-03 00:59:54 +0100

    added backpack and latest features from master branch
    this PR
    adds the following to the 4.0 dev branch
    - Made XRToolsSnapZone.pick_up_object a public method #401
    - added basic backpack inventory #402
    - added tool annotation to snap_zone.gd #405

---
+++ addons/godot-xr-tools/objects/inventory/inventory_backpack.gd
@@ -0,0 +1,86 @@
+@icon("res://addons/godot-xr-tools/editor/icons/backpack.svg")
+@tool
+class_name XRToolsInventoryBackpack
+extends Node
+
+# if set to true, backpack will return on drop after
+# wait time passed
+@export var return_to_player : bool = false
+
+# wait time for return to player bool
+@export var wait_time = 3
+
+@export var backpack_path: NodePath
+
+# timer node to control the return to player time
+var timer
+
+# Array of all snap-zones in the backpack
+var _backpack_snap_zones := []
+
+@onready var backpack: XRToolsPickable = get_node(backpack_path)
+
+# Add support for is_xr_class on XRTools classes
+func is_xr_class(name : String) -> bool:
+	return name == "XRToolsInventoryBackpack"
+
+
+func _ready():
+	timer = get_node("Timer")
+	timer.timeout.connect(_on_Timer_timeout)
+
+	# Listen to when this backpack is picked up or dropped
+	backpack.picked_up.connect(_on_backpack_picked_up)
+	backpack.dropped.connect(_on_backpack_dropped)
+
+	# Get all the snap-zones
+	_backpack_snap_zones = XRTools.find_xr_children(backpack, "*", "XRToolsSnapZone")
+
+	# Disable all snap-zones in this backpack
+	for snap in _backpack_snap_zones :
+		snap.enabled = false
+
+
+func _on_backpack_picked_up(_pickable):
+	# Enable all snap-zones when picked up by a hand/controller
+	if backpack.get_picked_up_by_controller():
+		for snap in _backpack_snap_zones:
+			snap.enabled = true
+
+func _on_backpack_dropped(_pickable):
+	# Disable all snap-zones when the backpack is dropped
+	for snap in _backpack_snap_zones :
+		snap.enabled = false
+
+	if !return_to_player:
+		# example code to automatically return backpack
+		# to player holster when dropped
+			if _pickable.picked_up_by == null and _pickable.is_picked_up() == false:
+				var shoulder_holster = get_tree().get_nodes_in_group("ShoulderHolster")
+
+				if shoulder_holster == null:
+					return
+
+				for holster in shoulder_holster:
+				#put on empty shoulder slot if one is available
+					if holster.picked_up_object == null:
+						holster.pick_up_object(backpack)
+
+	if return_to_player:
+		# example code to automatically return backpack
+		# to player holster when dropped after wait time passed
+		if _pickable.picked_up_by == null and _pickable.is_picked_up() == false:
+			timer.start(wait_time)
+
+
+func _on_Timer_timeout():
+	var shoulder_holster = get_tree().get_nodes_in_group("ShoulderHolster")
+
+	if shoulder_holster == null:
+		return
+
+	for holster in shoulder_holster:
+	#put on empty shoulder slot if one is available
+		if holster.picked_up_object == null:
+			holster.pick_up_object(backpack)
+			break
