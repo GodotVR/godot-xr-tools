43f4a760a165ebe69f747630c3119db31fed549b
Malcolm Nixon <Malcolm.nixon@gmail.com>
2023-04-14 13:32:40 -0400

    Backport teleport beam options from master into 3.x maintenance branch.

--- assets/meshes/teleport/teleport.gd
+++ assets/meshes/teleport/teleport.gd
@@ -13,7 +13,16 @@ export (String, FILE, '*.tscn') var scene : String
 export var title: Texture setget _set_title
 
 ## Can Teleporter be used
-export var active := true
+export var active : bool = true setget _set_active
+
+## Is teleport beam visible if inactive
+export var inactive_beam_visible: bool = false setget _set_inactive_beam_visible
+
+## The beam color in active state
+export var active_beam_color: Color = Color("#2b40f8") setget _set_active_beam_color
+
+## The beam color in inactive state
+export var inactive_beam_color: Color = Color("#ad0400") setget _set_inactive_beam_color
 
 
 # Scene base to trigger loading
@@ -22,6 +31,7 @@ onready var _scene_base: XRToolsSceneBase = get_node(scene_base)
 
 func _ready():
 	_update_title()
+	_update_teleport()
 
 
 # Called when the player enters the teleport area
@@ -59,3 +69,31 @@ func _update_title():
 	if title:
 		var material: ShaderMaterial = $TeleportBody/Top.get_active_material(1)
 		material.set_shader_param("Title", title)
+
+func _set_active(value):
+	active = value
+	if is_inside_tree():
+		_update_teleport()
+
+func _set_active_beam_color(value):
+	active_beam_color = value
+	if is_inside_tree():
+		_update_teleport()
+
+func _set_inactive_beam_color(value):
+	inactive_beam_color = value
+	if is_inside_tree():
+		_update_teleport()
+
+func _set_inactive_beam_visible(value):
+	inactive_beam_visible = value
+	if is_inside_tree():
+		_update_teleport()
+
+func _update_teleport():
+	if active:
+		$TeleportArea/Cylinder.get_surface_material(0).set_shader_param("beam_color", active_beam_color)
+		$TeleportArea/Cylinder.visible = true
+	else:
+		$TeleportArea/Cylinder.get_surface_material(0).set_shader_param("beam_color", inactive_beam_color)
+		$TeleportArea/Cylinder.visible = inactive_beam_visible
