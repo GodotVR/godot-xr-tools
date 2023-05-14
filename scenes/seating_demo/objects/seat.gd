88f14b503e83e196f09b729ce712883e5ae96055
pietru <jabartek2004.bob@gmail.com>
2023-05-14 23:40:05 +0200

    bux fix and update of seat

--- scenes/seating_demo/objects/seat.gd
+++ scenes/seating_demo/objects/seat.gd
@@ -10,10 +10,12 @@ func _ready() -> void:
 	last_chair_rot=self.global_rotation
 	
 	#Assume XROrigin is named XROrigin3D and is accessible via unical name
-	XROrigin = get_node("%XROrigin3D")
+	var sb = XRTools.find_xr_ancestor(self,"*","XRToolsSceneBase")
+	XROrigin = XRTools.find_xr_child(sb,"*","XROrigin3D")
+
 	#Get Playerbody
 	PlayerBody = XROrigin.get_node("PlayerBody")
-	
+
 	#Check and Retrive exit node if it exists
 	if has_node("Exit"):
 		Exit_Node = get_node("Exit")
@@ -28,8 +30,6 @@ var is_sitting := false
 #For multiplayer to set should chair update local plr pos
 var is_local := false
 
-@export var seating_rot_correction := Vector3.ZERO
-
 func seat(local:=true):
 	if !is_sitting:
 		is_sitting=true
@@ -40,9 +40,14 @@ func seat(local:=true):
 			
 			PlayerBody.set_enabled(false)
 			
+			if has_node("CamPos"):
+				XROrigin.global_transform.origin=get_node("CamPos").global_transform.origin
+				XROrigin.global_rotation=get_node("CamPos").global_rotation
+				return
+			
 			XROrigin.global_transform.origin=self.global_transform.origin
-			XROrigin.global_rotation=self.global_rotation+seating_rot_correction
-		
+			XROrigin.global_rotation=self.global_rotation
+			XRServer.center_on_hmd(XRServer.DONT_RESET_ROTATION,true)
 
 @export_range(-1,100) var max_last_pos_exit := 6
 func unseat():
@@ -60,18 +65,18 @@ func unseat():
 
 var last_chair_pos := Vector3.ZERO
 var last_chair_rot := Vector3.ZERO
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if is_sitting and is_local:
 		#calc seat pos change
 		var mov :Vector3= self.global_transform.origin-last_chair_pos
 		var rot :Vector3= self.global_rotation-last_chair_rot
 		
 		#Update player position if seat global position changed
-		if !mov.is_equal_approx(Vector3.ZERO):
-			XROrigin.global_transform.origin+=mov
-		#Update player rotation if seat global rotation changed
-		if !rot.is_equal_approx(Vector3.ZERO):
-			XROrigin.global_rotation+=rot
+		if !mov.is_equal_approx(Vector3.ZERO) or !rot.is_equal_approx(Vector3.ZERO):
+			if has_node("CamPos"):
+				XROrigin.global_transform=get_node("CamPos").global_transform
+			else:
+				XROrigin.global_transform=self.global_transform
 		
 		#Store current position
 		last_chair_pos=self.global_transform.origin
