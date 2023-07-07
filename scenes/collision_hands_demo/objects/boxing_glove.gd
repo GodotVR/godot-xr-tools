2312d0d99e6c91ef02f9386cfe7ee0949f52f94e
D@re <56046022+DigitalN8m4r3@users.noreply.github.com>
2023-07-08 00:25:48 +0200

    added boxing gloves - collision toy
    adds boxing gloves to the collision demo as pickables, they got a child script that is responsible for adjusting depending on which controller picked it up
    
    bonus: added the gloves as xr tools hands
    this might come in handy, maybe for the rumble demo?

---
+++ scenes/collision_hands_demo/objects/boxing_glove.gd
@@ -0,0 +1,34 @@
+extends Node
+
+@export var left_glove : Node3D
+@export var right_glove : Node3D
+@export var c : CollisionShape3D
+
+@onready var _parent : XRToolsPickable = get_parent()
+
+func _ready():
+	_parent.picked_up.connect(_on_picked_up)
+	if left_glove.visible:
+		c.position = Vector3(0.045, 0, -0.092)
+	else:
+		c.position = Vector3(-0.045, 0, -0.092)
+	
+# Called when this object is picked up
+func _on_picked_up(_pickable) -> void:
+	_parent.by_controller = _parent.get_picked_up_by_controller()
+	if _parent.by_controller.name.matchn("*left*"):
+		if left_glove.visible:
+			return
+			
+		if !left_glove.visible:
+			c.position = Vector3(0.045, 0, -0.092)
+			right_glove.visible = false
+			left_glove.visible = true
+	else:
+		if right_glove.visible:
+			return
+			
+		if !right_glove.visible:
+			c.position = Vector3(-0.045, 0, -0.092)
+			right_glove.visible = true
+			left_glove.visible = false
