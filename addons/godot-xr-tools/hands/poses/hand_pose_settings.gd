828241f57310501a2a62189b265efa3e7d5f66c4
Malcolm Nixon <Malcolm.nixon@gmail.com>
2023-12-10 21:37:28 -0500

    Automatically update hand poses in the editor
    This PR updates the editor-preview hand-pose when changing the pose resource. As such the developer does not have to toggle between open/closed preview to see the updated hand pose.

--- addons/godot-xr-tools/hands/poses/hand_pose_settings.gd
+++ addons/godot-xr-tools/hands/poses/hand_pose_settings.gd
@@ -1,3 +1,4 @@
+@tool
 @icon("res://addons/godot-xr-tools/editor/icons/hand.svg")
 class_name XRToolsHandPoseSettings
 extends Resource
@@ -10,7 +11,19 @@ extends Resource
 
 
 ## Hand-open pose
-@export var open_pose : Animation
+@export var open_pose : Animation : set = set_open_pose
 
 ## Hand-closed pose
-@export var closed_pose : Animation
+@export var closed_pose : Animation : set = set_closed_pose
+
+
+# Called when the open pose is changed
+func set_open_pose(p_open_pose : Animation) -> void:
+	open_pose = p_open_pose
+	emit_changed()
+
+
+# Called when the closed pose is changed
+func set_closed_pose(p_closed_pos : Animation) -> void:
+	closed_pose = p_closed_pos
+	emit_changed()
