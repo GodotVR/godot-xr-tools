f4f4aae03c884fe508c85be707aa4765a55160cd
squid <squidassasin23@gmail.com>
2025-11-11 18:07:49 -0700

    fix: shadowed variables

--- assets/digitaln8m4r3/scripts/firearm_bolt.gd
+++ assets/digitaln8m4r3/scripts/firearm_bolt.gd
@@ -13,8 +13,8 @@ var _current_controller : XRController3D
 
 
 # Add support for is_xr_class on XRTools classes
-func is_xr_class(name : String) -> bool:
-	return name == "XRFirearmBolt"
+func is_xr_class(xr_name : String) -> bool:
+	return xr_name == "XRFirearmBolt"
 
 
 func _ready() -> void:
