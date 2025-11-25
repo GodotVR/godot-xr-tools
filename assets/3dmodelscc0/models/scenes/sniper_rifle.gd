b9c300809b3808e73d7d9e17eeaf059d4e375fa1
Bastiaan Olij <mux213@gmail.com>
2025-11-25 12:13:07 +1100

    Fix some sniper riffle issues and another player body issue

--- assets/3dmodelscc0/models/scenes/sniper_rifle.gd
+++ assets/3dmodelscc0/models/scenes/sniper_rifle.gd
@@ -1,7 +1,21 @@
 @tool
 extends XRToolsPickable
 
+@onready var sniper_handle : XRToolsInteractableHandle = $sniper_rifle/FirearmSlide/HandleOrigin/InteractableHandle
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	super._ready()
+
+	# Disable by default
+	sniper_handle.enabled = false
+
+
+func _on_picked_up(pickable):
+	# Enable on pickup
+	sniper_handle.enabled = true
+
+
+func _on_dropped(pickable):
+	# Disable on drop
+	sniper_handle.enabled = false
