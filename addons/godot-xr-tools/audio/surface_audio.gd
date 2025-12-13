984f3588afa3bbbf1d7bed53360010b9c6f36028
Mubinul Haque <80097192+mubinulhaque@users.noreply.github.com>
2025-12-13 17:23:35 -0100

    Better format Surface Audio

--- addons/godot-xr-tools/audio/surface_audio.gd
+++ addons/godot-xr-tools/audio/surface_audio.gd
@@ -3,29 +3,27 @@
 class_name XRToolsSurfaceAudio
 extends Node
 
-
 ## XRTools Surface Audio Node
 ##
 ## This node is attached as a child of a StaticObject to give it a surface
 ## audio type. This will cause the XRToolsMovementFootStep to play the correct
 ## foot-step sounds when walking on the object.
 
-
 ## XRToolsSurfaceAudioType to associate with this surface
-@export var surface_audio_type : XRToolsSurfaceAudioType
+@export var surface_audio_type: XRToolsSurfaceAudioType
 
 
-# Add support for is_class on XRTools classes
-func is_xr_class(xr_name:  String) -> bool:
+## Add support for is_class on XRTools classes
+func is_xr_class(xr_name: String) -> bool:
 	return xr_name == "XRToolsSurfaceAudio"
 
 
-# This method checks for configuration issues.
+# Checks for configuration issues.
 func _get_configuration_warnings() -> PackedStringArray:
 	var warnings := PackedStringArray()
 
 	# Verify the camera
-	if !surface_audio_type:
+	if not surface_audio_type:
 		warnings.append("Surface audio type not specified")
 
 	# Return warnings
