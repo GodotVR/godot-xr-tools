db816b547b6969d102954eb714c91baef69850d0
Mubinul Haque <80097192+mubinulhaque@users.noreply.github.com>
2025-12-13 17:13:32 -0100

    Better format and static type Area Audio

--- addons/godot-xr-tools/audio/area_audio.gd
+++ addons/godot-xr-tools/audio/area_audio.gd
@@ -2,8 +2,6 @@
 @icon("res://addons/godot-xr-tools/editor/icons/audio.svg")
 class_name XRToolsAreaAudio
 extends AudioStreamPlayer3D
-
-
 ## XRTools Area Audio
 ##
 ## This node is attached as a child of a Area3D,
@@ -12,17 +10,17 @@ extends AudioStreamPlayer3D
 
 
 ## XRToolsAreaAudioType to associate with this Area Audio
-@export var area_audio_type : XRToolsAreaAudioType
+@export var area_audio_type: XRToolsAreaAudioType
 
-@onready var area : Area3D = get_parent()
+@onready var area: Area3D = get_parent()
 
 
-# Add support for is_class on XRTools classes
-func is_xr_class(xr_name:  String) -> bool:
+## Add support for is_class on XRTools classes
+func is_xr_class(xr_name: String) -> bool:
 	return xr_name == "XRToolsAreaAudio"
 
 
-# Called when the node enters the scene tree for the first time.
+## Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Listen for enter
 	area.body_entered.connect(_on_body_entered)
@@ -30,19 +28,19 @@ func _ready() -> void:
 	area.body_exited.connect(_on_body_exited)
 
 
-func _on_body_entered(_body):
+func _on_body_entered(_body: Node3D) -> void:
 	if playing:
 		stop()
 	stream = area_audio_type.touch_sound
 	play()
 
 
-func _on_body_exited(_body):
+func _on_body_exited(_body: Node3D) -> void:
 	if playing:
 			stop()
 
 
-# This method checks for configuration issues.
+## This method checks for configuration issues.
 func _get_configuration_warnings() -> PackedStringArray:
 	var warnings := PackedStringArray()
 
