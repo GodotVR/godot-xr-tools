bb3f394da36269f6b7ab01e1259fae07eb56d15c
Mubinul Haque <80097192+mubinulhaque@users.noreply.github.com>
2026-02-17 11:52:14 +0000

    Better format Pickable Audio Type (#765)

--- addons/godot-xr-tools/audio/pickable_audio_type.gd
+++ addons/godot-xr-tools/audio/pickable_audio_type.gd
@@ -3,27 +3,25 @@
 class_name XRToolsPickableAudioType
 extends Resource
 
-
 ## XRTools Pickable Audio Type Resource
 ##
 ## This resource defines the audio streams to play when
 ## the pickable is being picked up/ dropped/ hit something while being held
 
-
 ## Surface name
-@export var name : String = ""
+@export var name: String = ""
 
 ## Optional audio stream to play when the player picks up the pickable
-@export var grab_sound : AudioStream
+@export var grab_sound: AudioStream
 
 ## Optional audio stream to play when the player drops the pickable
-@export var drop_sound : AudioStream
+@export var drop_sound: AudioStream
 
 ## Optional audio stream to play when the item is beign held by the player
-@export var hit_sound : AudioStream
+@export var hit_sound: AudioStream
 
 
-# This method checks for configuration issues.
+# Checks for configuration issues.
 func _get_configuration_warnings() -> PackedStringArray:
 	var warnings := PackedStringArray()
 
