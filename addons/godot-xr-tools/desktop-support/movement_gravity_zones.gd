7d58ac83bbce5cfc2dafe7f3c59ce2ff2aa829d7
Mubinul Haque <80097192+mubinulhaque@users.noreply.github.com>
2026-03-01 23:32:53 +0000

    Better format and static type Gravity Zones (#783)

--- addons/godot-xr-tools/desktop-support/movement_gravity_zones.gd
+++ addons/godot-xr-tools/desktop-support/movement_gravity_zones.gd
@@ -2,59 +2,77 @@
 class_name XRToolsMovementGravityZones
 extends XRToolsMovementProvider
 
-
-# Default wall-walk mask of 4:wall-walk
+## Default wall-walk mask of 4:wall-walk
 const DEFAULT_MASK := 0b0000_0000_0000_0000_0000_0000_0000_1000
 
 
 ## Wall walking provider order
-@export var order : int = 26
+@export var order: int = 26
 
 ## Set our follow layer mask
-@export_flags_3d_physics var follow_mask : int = DEFAULT_MASK
-
-var _gravity_dir := Vector3(0,-9.8,0)
-
-@onready var fly_desktop : XRToolsDesktopMovementFlight = XRTools.find_xr_child(
-	XRTools.find_xr_ancestor(self,"*","XROrigin3D"),
-	"*",
-	"XRToolsDesktopMovementFlight")
-
-@onready var fly_xr : XRToolsMovementFlight = XRTools.find_xr_child(
-	XRTools.find_xr_ancestor(self,"*","XROrigin3D"),
-	"*",
-	"XRToolsMovementFlight")
+@export_flags_3d_physics var follow_mask: int = DEFAULT_MASK
 
 
-func physics_pre_movement(_delta: float, player_body: XRToolsPlayerBody):
+# Direction of gravity
+var _gravity_dir: Vector3 = Vector3(0, -9.8, 0)
+
+
+## Desktop Flight node
+@onready var fly_desktop: XRToolsDesktopMovementFlight = XRTools.find_xr_child(
+		XRTools.find_xr_ancestor(
+				self,
+				"*",
+				"XROrigin3D",
+		),
+		"*",
+		"XRToolsDesktopMovementFlight",
+)
+
+## XR Flight node
+@onready var fly_xr: XRToolsMovementFlight = XRTools.find_xr_child(
+		XRTools.find_xr_ancestor(
+				self,
+				"*",
+				"XROrigin3D",
+		),
+		"*",
+		"XRToolsMovementFlight",
+)
+
+
+func physics_pre_movement(_delta: float, player_body: XRToolsPlayerBody) -> void:
 	# Test for collision with wall under feet
-	var gravity_zones1 = get_tree().get_nodes_in_group("gravity_zone1")
-	var gravity_zones2 = get_tree().get_nodes_in_group("gravity_zone2")
-	var gravity_zones3 = get_tree().get_nodes_in_group("gravity_zone3")
+	var gravity_zones1: Array[Node] = get_tree().get_nodes_in_group("gravity_zone1")
+	var gravity_zones2: Array[Node] = get_tree().get_nodes_in_group("gravity_zone2")
+	var gravity_zones3: Array[Node] = get_tree().get_nodes_in_group("gravity_zone3")
 
-	#_gravity_dir = Vector3(0,-9.8,0)
-	var grav_z := 0
+	var grav_z: int = 0
+
 	for zone in gravity_zones3:
 		if zone.overlaps_body(player_body) and zone is Area3D:
-			grav_z=3
-			_gravity_dir=zone.global_transform.basis.y*zone.gravity*-1
+			grav_z = 3
+			_gravity_dir = zone.global_transform.basis.y * zone.gravity * -1
 
-	if grav_z==0:
+	if grav_z == 0:
+		# If there are no Area3Ds in the group gravity_zone3
 		for zone in gravity_zones2:
 			if zone.overlaps_body(player_body) and zone is Area3D:
-				grav_z=2
-				_gravity_dir=zone.global_transform.basis.y*zone.gravity*-1
+				grav_z = 2
+				_gravity_dir = zone.global_transform.basis.y * zone.gravity * -1
 
-	if grav_z==0:
+	if grav_z == 0:
+		# If there are no Area3Ds in the group gravity_zone2
 		for zone in gravity_zones1:
 			if zone.overlaps_body(player_body) and zone is Area3D:
-				grav_z=1
-				_gravity_dir=zone.global_transform.basis.y*zone.gravity*-1
+				grav_z = 1
+				_gravity_dir = zone.global_transform.basis.y * zone.gravity * -1
 
-	if grav_z==0:
+	if grav_z == 0:
+		# If there are no Area3Ds in the gravity_zone groups
 		fly_desktop.set_flying(true)
 		fly_xr.set_flying(true)
 	else:
+		# If there are Area3Ds in the gravity_zone groups
 		fly_desktop.set_flying(false)
 		fly_xr.set_flying(false)
 
