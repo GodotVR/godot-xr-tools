ba65a6a62bb1872a4ae7ddd23fd60f65550745f4
Mubinul Haque <80097192+mubinulhaque@users.noreply.github.com>
2026-06-29 19:58:50 +0100

    Better format and static type Highlight Material

--- addons/godot-xr-tools/objects/highlight/highlight_material.gd
+++ addons/godot-xr-tools/objects/highlight/highlight_material.gd
@@ -2,56 +2,62 @@
 class_name XRToolsHighlightMaterial
 extends Node
 
-
 ## Mesh to highlight
-@export var highlight_mesh_instance : NodePath
+@export var highlight_mesh_instance: NodePath
 
 ## Material to set
-@export var highlight_material : Resource
+@export var highlight_material: Resource
 
 
-var _original_materials = Array()
-var _highlight_mesh_instance: MeshInstance3D
+var _original_materials: Array[Material]
+var _highlight_mesh: MeshInstance3D
+var _parent: Node
 
 
-# Add support for is_xr_class on XRTools classes
-func is_xr_class(xr_name:  String) -> bool:
-	return xr_name == "XRToolsHighlightMaterial"
+# When the node enters the scene tree for the first time.
+func _ready() -> void:
+	# Get the parent
+	_parent = get_parent()
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
 	# Get the mesh to highlight
-	_highlight_mesh_instance = get_node(highlight_mesh_instance)
+	_highlight_mesh = get_node(highlight_mesh_instance)
 
 	# Save the materials
-	if _highlight_mesh_instance:
-		# if we can find a node remember which materials are currently set on each surface
-		for i in range(0, _highlight_mesh_instance.get_surface_override_material_count()):
-			_original_materials.push_back(_highlight_mesh_instance.get_surface_override_material(i))
+	if _highlight_mesh:
+		# if we can find a node,
+		# remember which materials are currently set on each surface
+		for i: int in _highlight_mesh.get_surface_override_material_count():
+			_original_materials.push_back(
+					_highlight_mesh.get_surface_override_material(i)
+			)
 
 	# Hook the highlight update
-	get_parent().connect("highlight_updated", _on_highlight_updated)
+	if _parent.has_signal("highlight_updated"):
+		_parent.highlight_updated.connect(_on_highlight_updated)
+
+
+## Adds support for [method is_xr_class] on XRTools classes
+func is_xr_class(xr_name: String) -> bool:
+	return xr_name == "XRToolsHighlightMaterial"
 
 
 # Called when the pickable highlight changes
 func _on_highlight_updated(_pickable, enable: bool) -> void:
 	# Set the materials
-	if _highlight_mesh_instance:
-		for i in range(0, _highlight_mesh_instance.get_surface_override_material_count()):
+	if _highlight_mesh:
+		for i: int in _highlight_mesh.get_surface_override_material_count():
 			if enable:
-				_highlight_mesh_instance.set_surface_override_material(i, highlight_material)
+				_highlight_mesh.set_surface_override_material(i, highlight_material)
 			else:
-				_highlight_mesh_instance.set_surface_override_material(i, _original_materials[i])
+				_highlight_mesh.set_surface_override_material(i, _original_materials[i])
 
 
-# This method verifies the node
+# Verifies the node
 func _get_configuration_warnings() -> PackedStringArray:
 	var warnings := PackedStringArray()
 
 	# Verify parent supports highlighting
-	var parent := get_parent()
-	if not parent or not parent.has_signal("highlight_updated"):
+	if not _parent or not _parent.has_signal("highlight_updated"):
 		warnings.append("Parent does not support highlighting")
 
 	return warnings
