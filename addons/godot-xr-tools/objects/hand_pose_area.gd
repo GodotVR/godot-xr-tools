7e8fafd6ce805d0e4c1e569e37de1dab85f16e8f
Mubinul Haque <80097192+mubinulhaque@users.noreply.github.com>
2026-06-30 10:38:43 +0100

    Better format and static type Hand Pose Area

--- addons/godot-xr-tools/objects/hand_pose_area.gd
+++ addons/godot-xr-tools/objects/hand_pose_area.gd
@@ -3,35 +3,37 @@
 class_name XRToolsHandPoseArea
 extends Area3D
 
-
 ## XR Tools Hand Pose Area
 ##
-## This area works with the XRToolsFunctionPoseArea to control the pose
+## This area works with the [XRToolsFunctionPoseArea] to control the pose
 ## of the VR hands.
 
 
 ## Priority level for this hand pose area
-@export var pose_priority : int
+@export var pose_priority: int
 
 ## Left hand pose settings (XRToolsHandPoseSettings)
-@export var left_pose : XRToolsHandPoseSettings
+@export var left_pose: XRToolsHandPoseSettings
 
 ## Right hand pose settings (XRToolsHandPoseSettings)
-@export var right_pose : XRToolsHandPoseSettings
+@export var right_pose: XRToolsHandPoseSettings
 
 ## Array of grabpoints this hand pose area disables when active
-@export var grabpoints : Array[XRToolsGrabPointHand]
+@export var grabpoints: Array[XRToolsGrabPointHand]
 
-# Add support for is_xr_class on XRTools classes
-func is_xr_class(xr_name:  String) -> bool:
+
+## Add support for [method is_xr_class] on XRTools classes
+func is_xr_class(xr_name: String) -> bool:
 	return xr_name == "XRToolsHandPoseArea"
 
-# Disables grabpoints
-func disable_grab_points():
-	for grabpoint in grabpoints:
+
+## Disables grabpoints
+func disable_grab_points() -> void:
+	for grabpoint: XRToolsGrabPointHand in grabpoints:
 		grabpoint.enabled = false
 
-# Enables grabpoints
-func enable_grab_points():
-	for grabpoint in grabpoints:
+
+## Enables grabpoints
+func enable_grab_points() -> void:
+	for grabpoint: XRToolsGrabPointHand in grabpoints:
 		grabpoint.enabled = true
