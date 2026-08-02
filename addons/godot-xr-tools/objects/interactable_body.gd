f7c40e6316451eb9ce3c353cfa5f907c705f6520
Mubinul Haque <80097192+mubinulhaque@users.noreply.github.com>
2026-08-02 09:43:44 +0100

    Better format and static type Interactable Body (#859)

--- addons/godot-xr-tools/objects/interactable_body.gd
+++ addons/godot-xr-tools/objects/interactable_body.gd
@@ -1,7 +1,11 @@
 class_name XRToolsInteractableBody
 extends Node3D
-# This should extend from PhysicsBody3D but https://github.com/godotengine/godot/issues/46073
+## [PhysicsBody3D] that emits [XRToolsPointerEvent]s when pointed at by
+## [XRToolsFunctionPointer]s.
+##
+## Note: This should extend from [PhysicsBody3D],
+## but [url]https://github.com/godotengine/godot/issues/46073[/url]
 
 
-## Signal when pointer event occurs on body
-signal pointer_event(event)
+## Emitted when pointer event occurs on body
+signal pointer_event(event: XRToolsPointerEvent)
