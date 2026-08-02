16110b30099711d059cb6dc99a81830b3fa41f93
Mubinul Haque <80097192+mubinulhaque@users.noreply.github.com>
2026-08-02 09:49:57 +0100

    Better format and static type Highlight Visible (#852)

--- addons/godot-xr-tools/objects/highlight/highlight_visible.gd
+++ addons/godot-xr-tools/objects/highlight/highlight_visible.gd
@@ -2,34 +2,39 @@
 class_name XRToolsHighlightVisible
 extends Node3D
 
-
-# Add support for is_xr_class on XRTools classes
-func is_xr_class(xr_name:  String) -> bool:
-	return xr_name == "XRToolsHighlightVisible"
+var _parent: Node
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
+# When the node enters the scene tree for the first time.
+func _ready() -> void:
+	# Get the parent node
+	_parent = get_parent()
+
 	# Turn off until requested
 	if not Engine.is_editor_hint():
 		visible = false
 
 	# Hook the highlight update
-	get_parent().connect("highlight_updated", _on_highlight_updated)
+	if _parent.has_signal("highlight_updated"):
+		_parent.highlight_updated.connect(_on_highlight_updated)
 
 
-# Called when the pickable highlight changes
-func _on_highlight_updated(_pickable, enable: bool) -> void:
-	visible = enable
-
-
-# This method verifies the node
+# Verifies the node
 func _get_configuration_warnings() -> PackedStringArray:
 	var warnings := PackedStringArray()
 
 	# Verify parent supports highlighting
-	var parent := get_parent()
-	if not parent or not parent.has_signal("highlight_updated"):
+	if not _parent or not _parent.has_signal("highlight_updated"):
 		warnings.append("Parent does not support highlighting")
 
 	return warnings
+
+
+## Adds support for [method is_xr_class] on XRTools classes
+func is_xr_class(xr_name: String) -> bool:
+	return xr_name == "XRToolsHighlightVisible"
+
+
+# When the pickable highlight changes
+func _on_highlight_updated(_pickable: XRToolsPickable, enable: bool) -> void:
+	visible = enable
