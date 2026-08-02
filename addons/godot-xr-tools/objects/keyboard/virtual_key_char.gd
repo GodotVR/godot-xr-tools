858c59583fa52bb4d5d548b1d71e4e8a6f1fd546
Mubinul Haque <80097192+mubinulhaque@users.noreply.github.com>
2026-08-02 09:48:17 +0100

    Better format and static type Virtual Key Char (#855)

--- addons/godot-xr-tools/objects/keyboard/virtual_key_char.gd
+++ addons/godot-xr-tools/objects/keyboard/virtual_key_char.gd
@@ -14,32 +14,33 @@ extends XRToolsVirtualKey
 
 
 # Keyboard associated with this button
-var _keyboard : XRToolsVirtualKeyboard2D
+var _keyboard: XRToolsVirtualKeyboard2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
+# When the node enters the scene tree for the first time.
+func _ready() -> void:
 	# Call the base
 	super()
 
 	# Find the keyboard
 	_keyboard = XRTools.find_xr_ancestor(
-		self,
-		"*",
-		"XRToolsVirtualKeyboard2D")
+			self,
+			"*",
+			"XRToolsVirtualKeyboard2D",
+	)
 
 	# Handle button presses
 	pressed.connect(_on_pressed)
 	released.connect(_on_released)
 
 
-# Handler for button pressed
+# When a button is pressed down
 func _on_pressed() -> void:
 	highlighted = true
 	if _keyboard:
 		_keyboard.on_key_pressed(scan_code_text, unicode, shift_modifier)
 
 
-# Handler for button released
+# When a button is released
 func _on_released() -> void:
 	highlighted = false
